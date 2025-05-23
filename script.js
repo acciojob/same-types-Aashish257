567f4ac3aca8dad3849326970a6dbe5842514f4c
Aashish Shilkande <83578134+Aashish257@users.noreply.github.com>
2025-05-23 14:03:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
 function isSameType(value1, value2) {
+
   if (typeof value1 !== typeof value2) {
     return false;
   }
 
-  // Special case for NaN (since NaN !== NaN)
+
   if (typeof value1 === 'number' && typeof value2 === 'number') {
-    if (isNaN(value1) && isNaN(value2)) {
-      return true;
+    const bothAreNaN = isNaN(value1) && isNaN(value2);
+    const oneIsNaN = isNaN(value1) || isNaN(value2);
+
+    // If both are NaN => true, if only one is => false
+    if (oneIsNaN) {
+      return bothAreNaN;
     }
   }
 
   return true;
 }
+
 
 
 // do not change the code below.
